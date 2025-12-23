aee92555c3dca77d31446b72aa2658482268ffc2
Berilkeskin <s240307043s240307043@ankarabilim.edu.tr>
2025-12-24 00:42:06 +0300

    düzenlemeler

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-    # ✨ RENDER'DA KALICI VERİTABANI İÇİN:
-    # Eğer Render veritabanı (PostgreSQL) varsa onu kullan, yoksa yerelde sqlite kullan.
-    database_url = os.environ.get("DATABASE_URL")
-    if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-    
-    SQLALCHEMY_DATABASE_URI = database_url or ("sqlite:///" + os.path.join(BASE_DIR, "database.db"))
+
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
